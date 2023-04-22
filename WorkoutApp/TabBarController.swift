3572494dde82db230b466fb603c150311f66b15c
Janibek <jano.vr@mail.ru>
2023-04-22 19:52:30 +0400

    fix TabBar

--- WorkoutApp/TabBarController.swift
+++ WorkoutApp/TabBarController.swift
@@ -41,30 +41,30 @@ final class TabBarController: UITabBarController {
         let settingsController = UIViewController()
         
         
-        let overviewNavigation = UINavigationController(rootViewController: overviewController)
-        let sessionNavigation = UINavigationController(rootViewController: sessionController)
-        let progressNavigation = UINavigationController(rootViewController: progressController)
-        let settingsNavigation = UINavigationController(rootViewController: settingsController)
+        let overviewNavigation = NavBarController(rootViewController: overviewController)
+        let sessionNavigation = NavBarController(rootViewController: sessionController)
+        let progressNavigation = NavBarController(rootViewController: progressController)
+        let settingsNavigation = NavBarController(rootViewController: settingsController)
         
         
-        overviewController.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.overview,
+        overviewNavigation.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.overview,
                                                      image: Resources.Images.TabBar.overview,
                                                      tag: Tabs.overview.rawValue)
-        sessionController.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.session,
+        sessionNavigation.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.session,
                                                      image: Resources.Images.TabBar.session,
                                                      tag: Tabs.session.rawValue)
-        progressController.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.progress,
+        progressNavigation.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.progress,
                                                      image: Resources.Images.TabBar.progress,
                                                      tag: Tabs.progress.rawValue)
-        settingsController.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.settings,
+        settingsNavigation.tabBarItem = UITabBarItem(title: Resources.Strings.TabBar.settings,
                                                      image: Resources.Images.TabBar.settings,
                                                      tag: Tabs.settings.rawValue)
         
         setViewControllers([
-            overviewController,
-            sessionController,
-            progressController,
-            settingsController
+            overviewNavigation,
+            sessionNavigation,
+            progressNavigation,
+            settingsNavigation
         ], animated: false)
     }
 }
